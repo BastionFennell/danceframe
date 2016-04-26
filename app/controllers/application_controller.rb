45edb65dcc99b17fc01e55461804e2e673078f0b
Bastion Fennell <bastionfennell@utexas.edu>
2016-04-25 19:52:18 -0500

    Clean up the event page, add white-space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
   before_filter :setup_facebook
 
   def setup_facebook
-    @facebook_oauth||= Koala::Facebook::OAuth.new
+    @facebook_oauth = Koala::Facebook::OAuth.new
+    @facebook_oauth ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
+
     @access_token = @facebook_oauth.get_app_access_token
     @secret_key = @facebook_oauth.app_secret
     @graph = Koala::Facebook::API.new(@access_token, @secret_key)
