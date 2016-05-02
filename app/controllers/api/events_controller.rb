14c7ebb6152664b36619a5b24f1ee0bf2ffa30d8
Bastion Fennell <bastionfennell@utexas.edu>
2016-05-02 16:17:39 -0500

    Fix index action in events_controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,5 +1,16 @@
 class Api::EventsController < ApplicationController
   include JSONAPI::ActsAsResourceController
+  def index
+    if params[:upcoming]
+      event = Event.where('start_time >= :date', date: Date.today).order('start_time ASC').take(3)
+      event_resources = event.map{ |e| Api::EventResource.new(e, nil)}
+      event_resources_json = JSONAPI::ResourceSerializer.new(Api::EventResource).serialize_to_hash(event_resources)
+      render json: event_resources_json
+    else
+      super
+    end
+  end
+
   def create
     facebook_event = @graph.get_object(params[:data][:attributes]["facebook-id"], {fields: ['name', 'description', 'end_time', 'start_time', 'cover']})
     event = Event.new({
