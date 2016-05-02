9b9acb89bafde2aba5724f0dec1ab76d3789b1f3
Marguerite West-Driga <mwestdri@gmail.com>
2016-05-01 22:04:19 -0500

    Fix some JSlint errors for events/new route

--- ember-app/app/routes/events/new.js
+++ ember-app/app/routes/events/new.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params){
-    event = this.store.createRecord("event", {
+    var new_event = this.store.createRecord("event", {
                      facebookId: params.id
-    })
-    event.save()
+    });
+    new_event.save();
   }
 });
