8008ce520878b57c81b057088bc47b773579dc04
Bastion Fennell <bastionfennell@utexas.edu>
2016-05-24 14:32:33 -0500

    try to minify ember, give a fallback when ads are blocked

--- ember-app/app/components/banner-ad.js
+++ ember-app/app/components/banner-ad.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  adBlockExists: false,
+
+  didInsertElement: function(){
+    this.set('adBlockExists', $('.banner-ad').css('display') === "none");
+  }
+
 });
