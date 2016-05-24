af63af1489bf12f9615973cb3be59e5c858d36fb
Bastion Fennell <bastionfennell@utexas.edu>
2016-05-24 16:39:28 -0500

    Make the remove button inline

--- ember-app/app/components/todo-input.js
+++ ember-app/app/components/todo-input.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'span',
+
   actions: {
     updateTodo: function(value){
       this.sendAction('updateTodo', value, this.get("index"));
